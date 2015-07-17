e9530f835ebd1cdc38dc6cdfdc036c8c2633e975
can-till-lever <github@oneunified.net>
2015-07-17 16:12:00 -0300

    couple more files

---
+++ Session.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   Session.h
+ * Author: sysadmin
+ * 
+ * Created on December 5, 2014, 5:54 PM
+ */
+
+#ifndef SESSION_H
+#define	SESSION_H
+
+class Session {
+public:
+  Session();
+  virtual ~Session();
+private:
+
+};
+
+#endif	/* SESSION_H */
+
